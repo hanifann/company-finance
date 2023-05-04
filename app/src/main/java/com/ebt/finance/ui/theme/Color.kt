d76fe45b30b62bb9ee60446b0cd3fcc71f8c903a
hanifan <hanifannurulhaq@outlook.com>
2023-05-04 11:54:24 +0700

    change color theme

--- app/src/main/java/com/ebt/finance/ui/theme/Color.kt
+++ app/src/main/java/com/ebt/finance/ui/theme/Color.kt
@@ -2,10 +2,6 @@ package com.ebt.finance.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Primary = Color(red = 244, green = 244, blue = 244)
+val Secondary = Color(red = 31, green = 34, blue = 37)
+val Accent = Color(red = 33, green = 80, blue = 219)
