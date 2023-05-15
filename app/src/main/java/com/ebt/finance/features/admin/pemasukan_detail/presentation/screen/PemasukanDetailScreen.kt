18ec18f6d8dc2e07549277c2c5c9440d8da54434
hanifann <hanifannurulhaq@outlook.com>
2023-05-15 16:48:50 +0700

    add error handling when image not found

--- app/src/main/java/com/ebt/finance/features/admin/pemasukan_detail/presentation/screen/PemasukanDetailScreen.kt
+++ app/src/main/java/com/ebt/finance/features/admin/pemasukan_detail/presentation/screen/PemasukanDetailScreen.kt
@@ -1,12 +1,18 @@
 package com.ebt.finance.features.admin.pemasukan_detail.presentation.screen
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,20 +21,31 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.ebt.finance.R
+import com.ebt.finance.common.Constant
 import com.ebt.finance.features.admin.pemasukan_detail.presentation.components.RowTextAndValueComponent
 import com.ebt.finance.features.admin.pemasukan_detail.presentation.viewmodel.PemasukanDetailViewModel
+import com.ebt.finance.features.image_viewer.presentation.domain.ImageViewer
 import com.ebt.finance.ui.theme.Accent
 import com.ebt.finance.ui.theme.Primary
 import com.ebt.finance.ui.theme.Subtitle
@@ -43,16 +60,18 @@ fun PemasukanDetailScreen(
     val state = viewModel.state.value
     val disState = viewModel.disState.value
 
+    var isImageError by remember { mutableStateOf(false) }
+
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(vertical = 16.dp)
             .background(
                 color = Primary
             ),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        items(state.data.data.size){
+        items(state.data.data.size) {
+            Spacer(modifier = Modifier.padding(vertical = 8.dp))
             Box(
                 modifier = Modifier
                     .background(
@@ -85,7 +104,7 @@ fun PemasukanDetailScreen(
                 fontSize = 24.sp
             )
             Text(
-                text = "Berikut meerupakan detail pemasukan",
+                text = "Berikut merupakan detail pemasukan",
                 fontSize = 14.sp
             )
             Box(
@@ -96,7 +115,7 @@ fun PemasukanDetailScreen(
                         shape = RoundedCornerShape(15.dp)
                     )
             ) {
-                Column (
+                Column(
                     modifier = Modifier.padding(horizontal = 16.dp)
                 ) {
                     RowTextAndValueComponent(
@@ -121,15 +140,19 @@ fun PemasukanDetailScreen(
                         modifier = Modifier
                             .padding(bottom = 8.dp)
                     )
-                    SimpleDateFormat("yyyy-MM-dd", Locale("id")).parse(state.data.data[0].tgl)?.let {
-                        RowTextAndValueComponent(
-                            title = "Tanggal",
-                            value = SimpleDateFormat("dd MMMM, yyyy", Locale("id")).format(it),
-                            modifier = Modifier
-                                .padding(bottom = 8.dp)
-                        )
-                    }
-                    SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale("id")).parse(state.data.data[0].updatedAt)?.let {
+                    SimpleDateFormat("yyyy-MM-dd", Locale("id")).parse(state.data.data[0].tgl)
+                        ?.let {
+                            RowTextAndValueComponent(
+                                title = "Tanggal",
+                                value = SimpleDateFormat("dd MMMM, yyyy", Locale("id")).format(it),
+                                modifier = Modifier
+                                    .padding(bottom = 8.dp)
+                            )
+                        }
+                    SimpleDateFormat(
+                        "yyyy-MM-dd HH:mm:ss",
+                        Locale("id")
+                    ).parse(state.data.data[0].updatedAt)?.let {
                         RowTextAndValueComponent(
                             title = "Jam",
                             value = SimpleDateFormat("HH:mm", Locale("id")).format(it),
@@ -137,8 +160,131 @@ fun PemasukanDetailScreen(
                                 .padding(bottom = 8.dp)
                         )
                     }
+                    Divider(
+                        color = Subtitle,
+                        thickness = 1.dp
+                    )
+                    Text(
+                        text = "Bukti pemasukan :",
+                        fontWeight = FontWeight(300),
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                        color = Color(red = 180, green = 180, blue = 180),
+                        modifier = Modifier
+                            .padding(vertical = 8.dp)
+                    )
+                    if (isImageError) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(200.dp)
+                                .background(
+                                    Color.LightGray,
+                                    shape = RoundedCornerShape(8.dp)
+                                ),
+                            contentAlignment = Alignment.Center,
+                            content = {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.baseline_broken_image_24),
+                                    contentDescription = "broken image",
+                                    tint = Color.White,
+                                    modifier = Modifier
+                                        .width(36.dp)
+                                        .height(36.dp)
+                                )
+                            }
+                        )
+                    }
+                    AsyncImage(
+                        model = "${Constant.BASE_URL}upload/pemasukan/${state.data.data[0].buktiPemasukan}",
+                        contentDescription = "bukti-pembayaran",
+                        contentScale = ContentScale.Crop,
+                        onError = {
+                            isImageError = true
+                        },
+                        modifier = Modifier
+                            .padding(top = 8.dp, bottom = 16.dp)
+                            .clip(
+                                RoundedCornerShape(8.dp)
+                            )
+                            .border(
+                                color = Subtitle,
+                                width = 1.dp,
+                                shape = RoundedCornerShape(8.dp)
+                            )
+                            .clickable(
+                                onClick = {
+                                    navController.navigate(
+                                        "image_viewer/${
+                                            viewModel.toJson(
+                                                image = ImageViewer(
+                                                    imageUrl = state.data.data[0].buktiPemasukan,
+                                                    category = "pemasukan"
+                                                )
+                                            )
+                                        }"
+                                    )
+                                }
+                            )
+
+                    )
                 }
             }
+            OutlinedButton(
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(8.dp),
+                border = BorderStroke(
+                    1.dp,
+                    Color.Red
+                ),
+                content = {
+                    Text(
+                        text = "Hapus pemasukan",
+                        color = Color.Red,
+                        fontWeight = FontWeight(500)
+                    )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
+            Spacer(modifier = Modifier.padding(vertical = 2.dp))
+            OutlinedButton(
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(8.dp),
+                border = BorderStroke(
+                    1.dp,
+                    Accent
+                ),
+                content = {
+                    Text(
+                        text = "Edit pemasukan",
+                        color = Accent,
+                        fontWeight = FontWeight(500)
+                    )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
+            Spacer(modifier = Modifier.padding(vertical = 2.dp))
+            ElevatedButton(
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.elevatedButtonColors(
+                    containerColor = Accent
+                ),
+                content = {
+                    Text(
+                        text = "Kembali",
+                        color = Color.White,
+                        fontWeight = FontWeight(500)
+                    )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
         }
     }
 
--- app/src/main/java/com/ebt/finance/features/admin/pengeluaran_detail/presentation/screen/PengeluaranDetailScreen.kt
+++ app/src/main/java/com/ebt/finance/features/admin/pengeluaran_detail/presentation/screen/PengeluaranDetailScreen.kt
@@ -1,12 +1,16 @@
 package com.ebt.finance.features.admin.pengeluaran_detail.presentation.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -17,16 +21,25 @@ import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.ebt.finance.R
+import com.ebt.finance.common.Constant
 import com.ebt.finance.features.admin.pemasukan_detail.presentation.components.RowTextAndValueComponent
 import com.ebt.finance.features.admin.pengeluaran_detail.presentation.viewmodel.PengeluaranDetailViewModel
 import com.ebt.finance.ui.theme.Accent
@@ -42,6 +55,10 @@ fun PengeluaranDetailScreen(
 ){
     val state = viewModel.state.value
     val disState = viewModel.jenisPengeluaranState.value
+
+    var isImageError by remember {
+        mutableStateOf(false)
+    }
 
     LazyColumn(
         modifier = Modifier
@@ -137,6 +154,60 @@ fun PengeluaranDetailScreen(
                                 .padding(bottom = 8.dp)
                         )
                     }
+                    Divider(
+                        color = Subtitle,
+                        thickness = 1.dp
+                    )
+                    Text(
+                        text = "Bukti pembayaran: ",
+                        fontWeight = FontWeight(300),
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                        color = Color(red = 180, green = 180, blue = 180),
+                        modifier = Modifier
+                            .padding(top = 8.dp)
+                    )
+                    if(isImageError){
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(200.dp)
+                                .background(
+                                    Color.LightGray,
+                                    shape = RoundedCornerShape(8.dp)
+                                ),
+                            contentAlignment = Alignment.Center,
+                            content = {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.baseline_broken_image_24),
+                                    contentDescription = "broken image",
+                                    tint = Color.White,
+                                    modifier = Modifier
+                                        .width(36.dp)
+                                        .height(36.dp)
+                                )
+                            }
+                        )
+                    }
+                    AsyncImage(
+                        model = "${Constant.BASE_URL}upload/pengeluaran/${state.data.data[0].buktiPengeluaran}",
+                        contentDescription = "bukti-pengeluaran",
+                        contentScale = ContentScale.Crop,
+                        onError = {
+                            isImageError = true
+                        },
+                        modifier = Modifier
+                            .padding(top = 8.dp, bottom = 16.dp)
+                            .clip(
+                                RoundedCornerShape(8.dp)
+                            )
+                            .border(
+                                color = Subtitle,
+                                width = 1.dp,
+                                shape = RoundedCornerShape(8.dp)
+                            )
+
+                    )
                 }
             }
         }
