3cb414a42542e03196044c8540f27d2a33ef5ba6
hanifann <hanifannurulhaq@outlook.com>
2023-05-15 16:47:02 +0700

    add new constant value

--- app/src/main/java/com/ebt/finance/common/Constant.kt
+++ app/src/main/java/com/ebt/finance/common/Constant.kt
@@ -1,9 +1,10 @@
 package com.ebt.finance.common
 
 object Constant {
-    const val BASE_URL = "http://keuangan.dlhcode.com/"
+    const val BASE_URL = "https://keuangan.dlhcode.com/"
     const val PARAM_INCOME_ID = "incomeId"
     const val PARAM_EXPANSE_ID = "expanseId"
     const val PARAM_DISTRIBUTOR = "distributor"
     const val PARAM_JENIS_PENGELUARAN = "jenisPengeluaran"
+    const val PARAM_IMG_URL = "imgUrl"
 }
