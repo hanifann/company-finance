8499f0d6e6a979427c92b6fac59b679d1abb42dd
hanifann <hanifannurulhaq@outlook.com>
2023-05-25 13:16:13 +0700

    edit postPemasukan function

--- app/src/main/java/com/ebt/finance/features/admin/tambah_data/data/datasources/TambahDataRemoteDataSource.kt
+++ app/src/main/java/com/ebt/finance/features/admin/tambah_data/data/datasources/TambahDataRemoteDataSource.kt
@@ -3,12 +3,15 @@ package com.ebt.finance.features.admin.tambah_data.data.datasources
 import arrow.core.Either
 import com.ebt.finance.common.FailedDto
 import com.ebt.finance.features.admin.tambah_data.data.Dto.DistributorDto
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
+import com.ebt.finance.features.admin.tambah_data.data.Dto.TambahPemasukaDto
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Headers
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface TambahDataRemoteDataSource {
 
@@ -17,26 +20,27 @@ interface TambahDataRemoteDataSource {
     suspend fun getDistributor(@Header("Authorization") token: String): Either<FailedDto, DistributorDto>
 
     @Headers("Accept: application/json")
-    @POST("/api/{tambah_pemasukan}")
-    @FormUrlEncoded
+    @POST("/api/tambah_pemasukan")
+    @Multipart
     suspend fun postPemasukan(
-        @Field("jenis_pemasukan") jenisPemasukan: String,
-        @Field("keterangan") keterangan: String,
-        @Field("tgl") tgl: String,
-        @Field("total_pemasukan") totalPemasukan: String,
-        @Field("bukti_pemasukan") bukti: String,
-        token: String,
-    ): Either<FailedDto, Void>
+        @Part("jenis_pemasukan") jenisPemasukan: RequestBody,
+        @Part("keterangan") keterangan: RequestBody,
+        @Part("tgl") tgl: RequestBody,
+        @Part("total_pemasukan") totalPemasukan: RequestBody,
+        @Part("distributor_id") distributorId: RequestBody,
+        @Part bukti: MultipartBody.Part,
+        @Header("Authorization") token: String
+    ): Either<FailedDto, TambahPemasukaDto>
 
-    @Headers("Accept: application/json")
-    @POST("/api/{tambah_pemasukan}")
-    @FormUrlEncoded
-    suspend fun postPengeluaran(
-        @Field("jenis_pengeluaran") jenisPengeluaran: String,
-        @Field("keterangan") keterangan: String,
-        @Field("tgl") tgl: String,
-        @Field("total_pengeluaran") totalPengeluaran: String,
-        @Field("bukti_pemasukan") bukti: String,
-        token: String,
-    ): Either<FailedDto, Void>
+//    @Headers("Accept: application/json")
+//    @POST("/api/{tambah_pengeluaran}")
+//    @FormUrlEncoded
+//    suspend fun postPengeluaran(
+//        @Field("jenis_pengeluaran") jenisPengeluaran: String,
+//        @Field("keterangan") keterangan: String,
+//        @Field("tgl") tgl: String,
+//        @Field("total_pengeluaran") totalPengeluaran: String,
+//        @Field("bukti_pemasukan") bukti: String,
+//        token: String,
+//    ): Either<FailedDto, Void>
 }
