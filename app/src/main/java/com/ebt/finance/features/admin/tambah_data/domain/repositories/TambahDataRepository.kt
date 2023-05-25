edea26a68385424ee64ac6588735cd455cda5cc7
hanifann <hanifannurulhaq@outlook.com>
2023-05-25 13:16:59 +0700

    change datatype for data variable, change right return data type

--- app/src/main/java/com/ebt/finance/features/admin/tambah_data/domain/repositories/TambahDataRepository.kt
+++ app/src/main/java/com/ebt/finance/features/admin/tambah_data/domain/repositories/TambahDataRepository.kt
@@ -3,10 +3,11 @@ package com.ebt.finance.features.admin.tambah_data.domain.repositories
 import arrow.core.Either
 import com.ebt.finance.common.FailedDto
 import com.ebt.finance.features.admin.tambah_data.data.Dto.DistributorDto
-import com.ebt.finance.features.admin.tambah_data.domain.model.TambahData
+import com.ebt.finance.features.admin.tambah_data.data.Dto.TambahPemasukaDto
+import com.ebt.finance.features.admin.tambah_data.domain.model.TambahDataBody
 
 interface TambahDataRepository {
     suspend fun getDistributor(token: String): Either<FailedDto, DistributorDto>
-    suspend fun postPemasukan(token: String, data: TambahData): Either<FailedDto, Void>
-    suspend fun postPengeluaran(token: String, data: TambahData): Either<FailedDto, Void>
+    suspend fun postPemasukan(token: String, data: TambahDataBody): Either<FailedDto, TambahPemasukaDto>
+//    suspend fun postPengeluaran(token: String, data: TambahData): Either<FailedDto, Void>
 }
