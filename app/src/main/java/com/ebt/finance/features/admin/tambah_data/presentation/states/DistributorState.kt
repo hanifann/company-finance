1c5ec1fd1784e96c0f534685c8dfaec1dd5e1e86
hanifann <hanifannurulhaq@outlook.com>
2023-05-25 13:14:44 +0700

    change val to var

--- app/src/main/java/com/ebt/finance/features/admin/tambah_data/presentation/states/DistributorState.kt
+++ app/src/main/java/com/ebt/finance/features/admin/tambah_data/presentation/states/DistributorState.kt
@@ -5,5 +5,5 @@ import com.ebt.finance.features.admin.tambah_data.domain.model.Distributor
 data class DistributorState(
     val isLoading: Boolean = false,
     val distributor: Distributor = Distributor(emptyList()),
-    val message: String = ""
+    var message: String = ""
 )
