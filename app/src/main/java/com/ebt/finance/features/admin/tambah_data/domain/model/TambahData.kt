fcbf47880b760c596c1e1bc806e958cd27f83d76
hanifann <hanifannurulhaq@outlook.com>
2023-05-25 13:15:18 +0700

    add tambah data class

--- app/src/main/java/com/ebt/finance/features/admin/tambah_data/domain/model/TambahData.kt
+++ app/src/main/java/com/ebt/finance/features/admin/tambah_data/domain/model/TambahData.kt
@@ -1,11 +1,14 @@
 package com.ebt.finance.features.admin.tambah_data.domain.model
 
 data class TambahData(
-    val jenisData: String,
     val keterangan: String,
     val tgl: String,
     val totalHarga: String,
     val bukti: String,
     val distributorId: String,
     val kategori: String
+)
+
+data class Tambah(
+    val data: TambahData
 )
