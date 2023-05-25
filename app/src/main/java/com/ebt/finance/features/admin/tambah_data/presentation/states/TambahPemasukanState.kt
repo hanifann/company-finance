e0654c70db39dbce926003b888f5d643c19067ca
hanifann <hanifannurulhaq@outlook.com>
2023-05-25 13:18:48 +0700

    add TambahPemasukanState

---
+++ app/src/main/java/com/ebt/finance/features/admin/tambah_data/presentation/states/TambahPemasukanState.kt
@@ -0,0 +1,18 @@
+package com.ebt.finance.features.admin.tambah_data.presentation.states
+
+import com.ebt.finance.features.admin.tambah_data.domain.model.Tambah
+import com.ebt.finance.features.admin.tambah_data.domain.model.TambahData
+
+data class TambahPemasukanState(
+    val loading: Boolean = false,
+    val data: Tambah = Tambah(TambahData(
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    )),
+    var message: String = "",
+    var isSuccess: Boolean = false
+)
