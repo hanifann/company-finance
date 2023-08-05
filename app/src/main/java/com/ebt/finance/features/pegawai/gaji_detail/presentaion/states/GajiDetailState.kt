fa5838ede5ba0f9600a19b421c590180810f8c54
hanifann <hanifannurulhaq@outlook.com>
2023-08-06 05:52:42 +0700

    change gaji datatype to GajiData

--- app/src/main/java/com/ebt/finance/features/pegawai/gaji_detail/presentaion/states/GajiDetailState.kt
+++ app/src/main/java/com/ebt/finance/features/pegawai/gaji_detail/presentaion/states/GajiDetailState.kt
@@ -1,9 +1,21 @@
 package com.ebt.finance.features.pegawai.gaji_detail.presentaion.states
 
-import com.ebt.finance.features.pegawai.gaji.domain.models.Gaji
+import com.ebt.finance.features.pegawai.gaji.domain.models.GajiData
 
 data class GajiDetailState(
     val isLoading: Boolean = false,
-    val gaji: Gaji = Gaji(emptyList()),
+    val gaji: GajiData = GajiData(
+        "",
+        "",
+        "",
+    "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    ),
     val error: String = ""
 )
